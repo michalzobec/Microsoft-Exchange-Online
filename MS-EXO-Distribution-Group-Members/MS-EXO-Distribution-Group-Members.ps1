325edcd7aea845c50702b657aeadf78d8edfbc21
Michal ZOBEC <michal.zobec.news@gmail.com>
2024-09-22 07:43:46 +0200

    Microsoft Exchange Online Distribution Group Members Update v24.09.22.073059
    Microsoft Exchange Online Distribution Group Members Update v24.09.22.073059

---
+++ MS-EXO-Distribution-Group-Members/MS-EXO-Distribution-Group-Members.ps1
@@ -0,0 +1,194 @@
+﻿#Requires -Version 7
+Set-StrictMode -Version 7
+
+<#
+.SYNOPSIS
+Microsoft Exchange Online Distribution Group Members Update
+Update Members in the Distribution Group on Microsoft Exchange Online service
+
+.DESCRIPTION
+Microsoft Exchange Online Distribution Group Members Update
+(c) 2020-2024 Michal Zobec, ZOBEC Consulting. All Rights Reserved.  
+web: www.michalzobec.cz, mail: michal@zobec.net  
+GitHub repository http://zob.ec/
+
+.OUTPUTS
+Only text output in console (yet).
+
+.EXAMPLE
+C:\> MS-EXO-Distribution-Group-Members.ps1
+
+.NOTES
+Twitter/X: @michalzobec
+Blog   : http://www.michalzobec.cz
+
+.LINK
+About this script on my Blog in Czech
+http://zob.ec/
+
+.LINK
+Documentation (ReadMe)
+https://github.com/michalzobec/
+
+.LINK
+Release Notes (ChangeLog)
+https://github.com/michalzobec/
+
+#>
+
+
+######
+$ScriptName = "Microsoft Exchange Online Distribution Group Members Update"
+$ScriptVersion = "24.09.22.073059"
+######
+
+
+######
+# External configuration file
+# $ConfigurationFileName = "Get-SystemReport-Config-Example.ps1"
+######
+
+$ScriptDir = (Split-Path $myinvocation.MyCommand.Path)
+
+# internal logging function
+Function Write-Log {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $False,
+            HelpMessage = "Select log level.")]
+        [ValidateSet("INFO", "WARN", "ERROR", "FATAL", "DEBUG")]
+        [String]
+        $Level = "INFO",
+
+        [Parameter(Mandatory = $True,
+            HelpMessage = "Information text to logfile.")]
+        [string]
+        $Message,
+
+        [Parameter(Mandatory = $False,
+            HelpMessage = "Logfilename and path.")]
+        [string]
+        $LogFile
+    )
+
+    $Stamp = Get-Date -Format "yyyy\/MM\/dd HH:mm:ss.fff"
+    $Line = "[$Stamp] [$Level] $Message"
+    If ($LogFile) {
+        Add-Content $LogFile -Value $Line
+    }
+    Else {
+        Write-Output $Line
+    }
+}
+
+
+# Definition of the log file - save method without subdirectory
+$LogDate = Get-Date -Format "yyyyMMdd"
+$LogFileName = "Get-SystemReport-log"
+$LogFile = $ScriptDir + "\$LogFileName-$LogDate.txt"
+$CopyRightYearFrom = "2016"
+$CopyRightYearTo = "2019"
+
+# Header
+Write-Host ""
+Write-Host "$ScriptName, version $ScriptVersion"
+Write-Host "(c) $CopyRightYearFrom-$CopyRightYearTo Michal Zobec, ZOBEC Consulting. All Rights Reserved."
+Write-Host ""
+Write-Host "Initializing script"
+
+$LogFileDir = $ScriptDir + "\logs"
+if (!(Test-Path $LogFileDir -pathType container)) {
+    Write-Verbose "Directory $LogFileDir was not found, creating."
+    Write-Log -LogFile $LogFile -Message "  Directory $LogFileDir was not found, creating."
+    New-Item $LogFileDir -type directory | Out-Null
+    if (!(Test-Path $LogFileDir -pathType container)) {
+        Write-Verbose "Directory $LogFileDir still not exist! Exiting."
+        Write-Log -LogFile $LogFile -Message "  Directory $LogFileDir still not exist! Exiting."
+        exit
+    }
+}
+
+# Redefinition of the log file with LogFileDir
+$LogFile = $LogFileDir + "\$LogFileName-$LogDate.txt"
+
+# $CfgFilePath = $ScriptDir + "\config\$ConfigurationFileName"
+# if (!(Test-Path $CfgFilePath)) {
+#     Write-Warning "File $ConfigurationFileName is required for run of this script! Exiting."
+#     Write-Log -LogFile $LogFile -Message "  File $ConfigurationFileName is required for run of this script! Exiting." -Level ERROR
+#     exit
+# }
+# Write-Host "Configuration file $ConfigurationFileName"
+# . $CfgFilePath
+
+# Custom Variables
+$excelPath = $ScriptDir + "Allowed-Senders-List\services-international.xlsx" # Cesta k Excel sešitu
+$distributionGroup = "exchange-senders-infrastructure-test@zobecint.cz" # Email distribučního seznamu
+$UserPrincipalName = "u7256yr@zobeccid.cz" # Tvé emailové uživatelské jméno pro přihlášení
+
+# Check if the required modules are installed
+if (-not (Get-Module -ListAvailable -Name ExchangeOnlineManagement)) {
+    Write-Host "Chyba: Modul 'ExchangeOnlineManagement' není nainstalován. Nainstalujte modul a zkuste to znovu." -ForegroundColor Red
+    exit
+}
+
+if (-not (Get-Module -ListAvailable -Name ImportExcel)) {
+    Write-Host "Chyba: Modul 'ImportExcel' není nainstalován. Nainstalujte modul a zkuste to znovu." -ForegroundColor Red
+    exit
+}
+
+# Check if the Excel file exists
+if (-not (Test-Path $excelPath)) {
+    Write-Host "Chyba: Soubor '$excelPath' neexistuje. Zkontrolujte cestu a zkuste to znovu." -ForegroundColor Red
+    exit
+}
+
+# Import the Exchange Online module
+Import-Module ExchangeOnlineManagement
+
+# Connect to Exchange Online using UserPrincipalName for modern authentication
+Connect-ExchangeOnline -UserPrincipalName $UserPrincipalName
+
+# Check if the distribution group exists
+if (-not (Get-DistributionGroup -Identity $distributionGroup -ErrorAction SilentlyContinue)) {
+    Write-Host "Chyba: Distribuční seznam '$distributionGroup' neexistuje. Zkontrolujte název a zkuste to znovu." -ForegroundColor Red
+    Disconnect-ExchangeOnline -Confirm:$false
+    exit
+}
+
+# Import the email addresses and display names from the Excel file
+$emailList = Import-Excel -Path $excelPath
+
+# Loop through each entry in the Excel and add missing ones to the distribution group
+foreach ($entry in $emailList) {
+    $email = $entry.Email
+    $displayName = $entry.'Display Name'
+    
+    # Check if the external contact exists
+    $externalContact = Get-MailContact -Identity $email -ErrorAction SilentlyContinue
+    
+    if (-not $externalContact) {
+        try {
+            # Create external contact if it doesn't exist
+            New-MailContact -Name $displayName -ExternalEmailAddress $email
+            Write-Host "Created external contact for $($displayName) with email $($email)"
+        } catch {
+            Write-Host "Chyba při vytváření kontaktu pro '$($displayName)': $_" -ForegroundColor Red
+            continue
+        }
+    }
+
+    # Now check if the email exists in the distribution group
+    if (-not (Get-DistributionGroupMember -Identity $distributionGroup | Where-Object { $_.PrimarySmtpAddress -eq $email })) {
+        try {
+            Add-DistributionGroupMember -Identity $distributionGroup -Member $email
+            Write-Host "Added $($email) to $distributionGroup"
+        } catch {
+            Write-Host "Failed to add $($email): $_"
+        }
+    } else {
+        Write-Host "$($email) is already a member of $distributionGroup"
+    }
+}
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline -Confirm:$false
